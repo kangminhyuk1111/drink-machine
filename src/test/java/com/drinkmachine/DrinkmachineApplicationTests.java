3c69468aaba40ff77a6393b476d22b9ed6d2c84e
kangminhyuk1111 <rkdglqkr12@naver.com>
2025-01-20 16:53:36 +0900

    first commit

---
+++ src/test/java/com/drinkmachine/DrinkmachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkmachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
